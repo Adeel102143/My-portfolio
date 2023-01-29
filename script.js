7b8bf3067246dd4f1f63810b004577778119147e
Adeel ahmed Munsha <117924594+Adeel102143@users.noreply.github.com>
2023-01-29 21:12:45 +0300

    uploaded from pc
    portfolio website

---
+++ script.js
@@ -0,0 +1,12 @@
+function openNav() {
+  document.getElementById("header").style.display = "block";
+  document.getElementById("menu").style.display="none";
+  
+
+}
+
+function closeNav() {
+  document.getElementById("header").style.display="none";
+  document.getElementById("menu").style.display="block";
+}
+
